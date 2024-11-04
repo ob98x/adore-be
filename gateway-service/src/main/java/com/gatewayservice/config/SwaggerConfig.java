cf69616ae4f44df0b1d8a574f6f99e12e4056ee2
KimYoungWoo <120362572+KimuYounguWoo@users.noreply.github.com>
2024-11-04 23:33:38 +0900

    Feat: MSA Swagger 추가 및 로드밸런서 설정 정리 (#8)

--- gateway-service/src/main/java/com/gatewayservice/config/SwaggerConfig.java
+++ gateway-service/src/main/java/com/gatewayservice/config/SwaggerConfig.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -29,6 +30,7 @@ public class SwaggerConfig {
                 .addList("Bearer Token");
 
         return new OpenAPI()
+                .addServersItem(new Server().url("/"))
                 .components(new Components().addSecuritySchemes("Bearer Token", apiKey))
                 .addSecurityItem(securityRequirement);
     }
